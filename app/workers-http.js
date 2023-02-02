c239b0f43bb4c11ad53aff51fa1ba47a912567eb
doroshuk89 <atlas.webdev89@gmail.com>
2023-02-02 13:07:56 +0300

    update

--- app/workers-http.js
+++ app/workers-http.js
@@ -1,12 +1,12 @@
 const { parentPort, workerData } = require("node:worker_threads");
 const http = require("http");
 
-parentPort.postMessage(`Start workers ${process.pid}`);
-console.log(workerData.port);
 const server = http
   .createServer((req, res) => {
     console.log("start");
   })
   .listen(workerData.port, () => {
-    console.log(`Server starting in port ${workerData.port}`);
+    console.log(
+      `Worker starting in port ${workerData.port}. Worker pid: ${process.pid}`
+    );
   });
