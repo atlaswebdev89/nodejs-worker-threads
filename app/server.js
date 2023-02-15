9e2066eae8c10261bd4e30bcebdbfa64f0d259f8
doroshuk89 <atlas.webdev89@gmail.com>
2023-02-15 12:27:17 +0300

    up

--- app/server.js
+++ app/server.js
@@ -3,50 +3,49 @@ const { cpus } = require("node:os");
 const { Worker, isMainThread } = require("node:worker_threads");
 const http = require("http");
 
-const workerScript = "./worker-http-get-request.js";
-
 env.config();
 const PORT = process.env.PORT || null;
 if (PORT === null) {
-  process.stdout.write("\nNot set port or not found .env file");
-  process.exit();
+    process.stdout.write("\nNot set port or not found .env file");
+    process.exit();
 }
 
 const numCpus = cpus().length;
 const workers = [];
 if (isMainThread) {
-  for (let i = 0; i < numCpus; i++) {
-    const worker = new Worker(__filename, {
-      workerData: "Hello",
-    });
-    workers.push(worker);
-    worker.on("message", (msg) => {
-      process.stdout.write(`${msg}\n`);
-    });
-    worker.on("error", (err) => {
-      process.stdout.write(err);
-    });
-    worker.on("exit", (code) => {
-      if (code !== 0) throw new Error(`Worker stopped with exit code ${code}`);
-    });
-  }
-  const router = async (req, res) => {
-    if (req.url === "/fib") {
-      // res.write("Waiting data...");
-      workers[0].postMessage(46);
-      // worker.postMessage(4);
-      workers[0].on("message", (msg) => {
-        res.end(JSON.stringify(msg));
-      });
-    } else {
-      res.write("Hello");
-      res.end("\n");
+    for (let i = 0; i < numCpus; i += 1) {
+        const worker = new Worker(__filename, {
+            workerData: "Hello",
+        });
+        workers.push(worker);
+        worker.on("message", (msg) => {
+            process.stdout.write(`${msg}\n`);
+        });
+        worker.on("error", (err) => {
+            process.stdout.write(err);
+        });
+        worker.on("exit", (code) => {
+            if (code !== 0)
+                throw new Error(`Worker stopped with exit code ${code}`);
+        });
     }
-  };
+    const router = async (req, res) => {
+        if (req.url === "/fib") {
+            // res.write("Waiting data...");
+            workers[0].postMessage(46);
+            // worker.postMessage(4);
+            workers[0].on("message", (msg) => {
+                res.end(JSON.stringify(msg));
+            });
+        } else {
+            res.write("Hello");
+            res.end("\n");
+        }
+    };
 
-  http.createServer(router).listen(PORT, () => {
-    process.stdout.write(`Server start in port ${PORT}\n`);
-  });
+    http.createServer(router).listen(PORT, () => {
+        process.stdout.write(`Server start in port ${PORT}\n`);
+    });
 } else {
-  const worker = require(workerScript);
+    require("./worker-http-get-request");
 }
--- app_workers/resize-worker.js
+++ app_workers/resize-worker.js
@@ -1,12 +1,12 @@
 module.exports = async function (input) {
-  const sharp = require("sharp");
-  const { src, height, width } = input;
-  const [filename, ext] = src.toString().split(".");
-  console.log(`Resizing ${src} to ${width}px wide`);
-  for (let i = 0; i < 5e10; i++) {}
-  (async () => {
-    await sharp(src)
-      .resize(width, height, { fit: "cover" })
-      .toFile(`${src}-${width}.${ext}`);
-  })();
+    const sharp = require("sharp");
+    const { src, height, width } = input;
+    const [filename, ext] = src.toString().split(".");
+    console.log(`Resizing ${src} to ${width}px wide`);
+    for (let i = 0; i < 5e10; i += 1) {}
+    (async () => {
+        await sharp(src)
+            .resize(width, height, { fit: "cover" })
+            .toFile(`${src}-${width}.${ext}`);
+    })();
 };
